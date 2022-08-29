15c884287729f533034afb065b237d97a9cc7baf
Towaki Takikawa <tovacinni@gmail.com>
2022-08-29 02:35:02 -0400

    Small patch to fix bug in interactive renderer
    Signed-off-by: Towaki Takikawa <tovacinni@gmail.com>

--- templates/template_app.py
+++ templates/template_app.py
@@ -83,7 +83,7 @@ class TemplateApp(WispApp):
         # See also wisp_state.graph.channels and wisp.core.channels.channels_starter_kit for configuring channels.
         # Options: Any subset of channel names defined in wisp_state.graph.channels
         wisp_state.renderer.available_canvas_channels = ["rgb", "depth", "my_latent"]
-        wisp_state.renderer.selected_canvas_channel = "RGB"  # Channel selected by default
+        wisp_state.renderer.selected_canvas_channel = "rgb"  # Channel selected by default
 
         # Lens mode for camera used to view the canvas.
         # Choices: 'perspective', 'orthographic'
