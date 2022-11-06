02c881cc4f685e69f8807676bd617f336afec79d
Katrin Schmid <kabischmid@gmail.com>
2022-11-06 21:30:24 +1100

    extension

--- setup.py
+++ setup.py
@@ -79,11 +79,10 @@ def get_extensions():
         print("No source files found for extension, skipping extension compilation")
         return None
 
-
+    extension = CUDAExtension
     if torch.cuda.is_available() or os.getenv('FORCE_CUDA', '0') == '1':
         define_macros += [("WITH_CUDA", None), ("THRUST_IGNORE_CUB_VERSION_CHECK", None)]
         sources += glob.glob('wisp/csrc/**/*.cu', recursive=True)
-        extension = CUDAExtension
         extra_compile_args.update({'nvcc': ['-O3']})
         #include_dirs = get_include_dirs()
     else:
