NVIDIAGameWorks/kaolin-wisp

commit ad6af84a4d6aac70542fa0639d32cae3ac90f5b5

Various bug fixes and quality improvements (#167)
List of changes:

1. New config arguments (especially in hash grid nerf) which improve
quality significantly
2. Adaptive ray batching (improves nerf quality a lot)
3. `bg_color` is now a tuple `(0.0, 0.0, 0.0)` not a string
4. Supporting other validation splits `--valid-split`
5. `apex` based fused Adam
6. Bug fix in rays for `dist_min`, inproper normalization in
`channel_fn`
7. 2D support for hash grid
8. `MultiTable` utility class for working with hash grids (and similar)
easier
9. App renderer will now try to make FPS stable for better UX 
10. Fixing iteration count
11. Fix tracker's tensorboard support

---------

Signed-off-by: Towaki Takikawa <tovacinni@gmail.com>
Co-authored-by: Towaki Takikawa <ttakikawa@nvidia.com>

wisp/renderer/core/render_core.py
@@ -8,6 +8,7 @@
 
 from __future__ import annotations
 import abc
+import math
 import numpy as np
 import torch
 import copy
@@ -52,7 +53,7 @@ class RendererCore:
         self._last_renderbuffer = None
 
         # Minimal resolution supported by RendererCore
-        self.MIN_RES = 128
+        self.MIN_RES = 64
 
     def _default_camera(self, lens="perspective"):
         # TODO: move defaults elsewhere
@@ -242,14 +243,34 @@ class RendererCore:
         res_x, res_y = self.res_x, self.res_y
 
         # If the FPS is slow, downscale the resolution for the render.
-        is_fps_lagging = time_delta is not None and \
-                         self.target_fps is not None and \
-                         self.target_fps > 0 and \
-                         time_delta > (1.0 / self.target_fps)
-        if self.interactive_mode and is_fps_lagging:
-            if res_x > self.MIN_RES and res_y > self.MIN_RES:
-               res_x //= 2
-               res_y //= 2
+
+        if self.interactive_mode:
+            #target_delta = 1.0 / self.target_interactive_fps
+            target_delta = 1.0 / 20.0
+        else:
+            #target_delta = 1.0 / self.target_static_fps
+            target_delta = 1.0 / 2.0
+
+        if 'res_x' in self._last_state:
+            num_pixels = self._last_state['res_x'] * self._last_state['res_y']
+        else:
+            num_pixels = res_x * res_y
+        time_per_pixel = time_delta / float(num_pixels)
+        target_num_pixels = target_delta / time_per_pixel
+        
+        screen_ratio = res_x / res_y
+        res = math.sqrt(target_num_pixels / screen_ratio)
+        res_x = min(res_x, int(math.floor(res * screen_ratio)))
+        res_y = min(res_y, int(math.floor(res)))
+        
+        if res_y < self.MIN_RES:
+            res_x = int(math.floor(self.MIN_RES*screen_ratio))
+            res_y = int(math.floor(self.MIN_RES))
+
+        if 'res_x' in self._last_state:
+            if abs(res_x - self._last_state['res_x']) < 10:
+                res_x = self._last_state['res_x']
+                res_y = self._last_state['res_y']
 
         # TODO(ttakikawa): Leaving a note here to think about whether this should be the case...
         # The renderer always needs depth, alpha, and rgb
@@ -377,10 +398,12 @@ class RendererCore:
             return True
 
         # Resolution check: if not full resolution - canvas is dirty
-        if self._last_state['res_x'] != self.camera.width or self._last_state['res_y'] != self.camera.height:
-            return True
+        #if self._last_state['res_x'] != self.camera.width or self._last_state['res_y'] != self.camera.height:
+        #    return True
 
         for att_name, prev_val in self._last_state.items():
+            if att_name in ['res_x', 'res_y']:
+                continue
             if not hasattr(self, att_name):
                 continue
             curr_val = self.__getattribute__(att_name)
@@ -432,7 +455,7 @@ class RendererCore:
         channels_kit = self.state.graph.channels
         channel_info = channels_kit.get(selected_output_channel, create_default_channel())
         normalized_channel = channel_info.normalize_fn(rb_channel.clone())  # Clone to protect from modifications
-
+        
         # To RGB (in normalized space)
         # TODO (operel): incorporate color maps
         channel_dim = normalized_channel.shape[-1]
@@ -460,10 +483,18 @@ class RendererCore:
     @camera.setter
     def camera(self, camera: Camera) -> None:
         self.state.renderer.selected_camera = camera
-
+    
     @property
     def target_fps(self) -> float:
         return self.state.renderer.target_fps
+
+    @property
+    def target_interactive_fps(self) -> float:
+        return self.state.renderer.target_interactive_fps
+    
+    @property
+    def target_static_fps(self) -> float:
+        return self.state.renderer.target_static_fps
 
     @property
     def interactive_mode(self) -> bool:

.gitignore
@@ -14,3 +14,4 @@ _*/
 build
 imgui.ini
 wandb/
+*.nsys-rep

app/nerf/configs/nerf_codebook.yaml
@@ -40,7 +40,7 @@ nef:  # Neural field: combines grid, decoders and positional embedders
 
 tracer: # A tracer for intersecting rays with the blas, generating samples along rays for the neural field & integrating
   constructor: 'PackedRFTracer'
-  bg_color: 'white'
+  bg_color: [ 1.0, 1.0, 1.0 ]
   num_steps: 16
   raymarch_type: 'voxel'
   step_size: 1.0
@@ -49,7 +49,7 @@ dataset:  # Train & validation dataset
   constructor: 'RTMVDataset'
   dataset_path: 'data/'
   split: 'train'      # Which subset of the dataset to use for training
-  bg_color: 'white'   # The background color to use for when alpha=0.
+  bg_color: [ 1.0, 1.0, 1.0 ]   # The background color to use for when alpha=0.
   mip: 2              # If provided, will rescale images by 2**mip. Useful when large images are loaded.
   dataset_num_workers: -1  # The number of workers to spawn for multiprocessed loading.
                            # If dataset_num_workers < 1, processing will take place on the main process.
@@ -94,6 +94,8 @@ tracker:
   enable_wandb: False
   tensorboard:                    # active when enable_tensorboard=True
     log_dir: '_results/logs/runs' # For TensorBoard summary
+    exp_name: null                # Only set this if you want to set an experiment set name specifically for TB
+    log_fname: null               # Only set this if you want to set a unique ID specifically for TB
   wandb:                          # active when enable_wandb=True
     project: 'wisp-nerf'
     entity: null                  # i.e. your wandb username here

app/nerf/configs/nerf_hash.yaml
@@ -19,11 +19,11 @@ grid: # Feature grid, the backbone of the neural field
     feature_dim: 2
     num_lods: 16
     multiscale_type: 'cat'
-    feature_std: 0.01
+    feature_std: 1.0e-09
     feature_bias:  0.0
     codebook_bitwidth: 19
     min_grid_res: 16
-    max_grid_res: 2048
+    max_grid_res: 512
 
 nef:  # Neural field: combines grid, decoders and positional embedders
   constructor: 'NeuralRadianceField'
@@ -36,14 +36,15 @@ nef:  # Neural field: combines grid, decoders and positional embedders
   layer_type: 'linear'
   hidden_dim: 64
   num_layers: 1
-  prune_density_decay: 0.6              # prune parameter for occupancy blas according to feature struct
+  bias: True
+  prune_density_decay: 0.95             # prune parameter for occupancy blas according to feature struct
   prune_min_density: 2.956033378250884  # prune parameter for occupancy blas according to feature struct
                                         # 2.956 = (0.01 * 512) / np.sqrt(3)
 
 tracer: # A tracer for intersecting rays with the blas, generating samples along rays for the neural field & integrating
   constructor: 'PackedRFTracer'
-  bg_color: 'white'
-  num_steps: 2048
+  bg_color: [ 0.0, 0.0, 0.0 ]
+  num_steps: 1024
   raymarch_type: 'ray'
   step_size: 1.0
 
@@ -51,7 +52,7 @@ dataset:  # Train & validation dataset
   constructor: 'NeRFSyntheticDataset'
   dataset_path: 'data/'
   split: 'train'      # Which subset of the dataset to use for training
-  bg_color: 'white'   # The background color to use for when alpha=0.
+  bg_color: [ 0.0, 0.0, 0.0 ]
   mip: 0              # If provided, will rescale images by 2**mip. Useful when large images are loaded.
   dataset_num_workers: -1  # The number of workers to spawn for multiprocessed loading.
                            # If dataset_num_workers < 1, processing will take place on the main process.
@@ -72,23 +73,26 @@ trainer:
   valid_every: 100         # Runs validation every N epochs
   enable_amp: True         # If enabled, the step() training function will use mixed precision.
   profile_nvtx: True       # If enabled, nvtx markers will be emitted by torch for profiling.
-  grid_lr_weight: 100.0  # Learning rate weighting applied only for the grid parameters (contain "grid" in their name)
+  grid_lr_weight: 500.0    # Learning rate weighting applied only for the grid parameters (contain "grid" in their name)
+  scheduler: True
 
   # MultiviewTrainer config
   prune_every: 100         # Invokes nef.prune() logic every "prune_every" iterations.
   random_lod: False        # If True, random LODs in the occupancy structure will be picked per step, during tracing.
                            # If False, will always used the highest level of the occupancy structure.
   rgb_lambda: 1.0          # Loss weight for rgb_loss component.
+  rgb_loss_type: 'huber'
+  rgb_loss_denom: 'rays'
 
   dataloader:
     batch_size: 1          # Number of images used per batch, for which dataset_transform.num_samples rays will be used
     num_workers: 0         # Number of cpu workers used to fetch data, always 0 here as data is already processed.
 
   optimizer:  # torch optimizer to use, and its args
-    constructor: 'RMSprop'
+    constructor: 'FusedAdam'
     lr: 0.001
-    eps: 1.0e-8
-    weight_decay: 0      # Applies to decoder params only
+    eps: 1.0e-16
+    weight_decay: 1.0e-06      # Applies to decoder params only
 
 tracker:
   log_dir: '_results/logs/runs'   # For logs and snapshots
@@ -96,6 +100,8 @@ tracker:
   enable_wandb: False
   tensorboard:                    # active when enable_tensorboard=True
     log_dir: '_results/logs/runs' # For TensorBoard summary
+    exp_name: null                # Only set this if you want to set an experiment set name specifically for TB
+    log_fname: null               # Only set this if you want to set a unique ID specifically for TB
   wandb:                          # active when enable_wandb=True
     project: 'wisp-nerf'
     entity: null                  # i.e. your wandb username here

app/nerf/configs/nerf_octree.yaml
@@ -39,7 +39,7 @@ nef:  # Neural field: combines grid, decoders and positional embedders
 
 tracer: # A tracer for intersecting rays with the blas, generating samples along rays for the neural field & integrating
   constructor: 'PackedRFTracer'
-  bg_color: 'white'
+  bg_color: [ 1.0, 1.0, 1.0 ]
   num_steps: 16
   raymarch_type: 'voxel'
   step_size: 1.0
@@ -48,7 +48,7 @@ dataset:  # Train & validation dataset
   constructor: 'RTMVDataset'
   dataset_path: 'data/'
   split: 'train'      # Which subset of the dataset to use for training
-  bg_color: 'white'   # The background color to use for when alpha=0.
+  bg_color: [ 1.0, 1.0, 1.0 ]   # The background color to use for when alpha=0.
   mip: 2              # If provided, will rescale images by 2**mip. Useful when large images are loaded.
   dataset_num_workers: -1  # The number of workers to spawn for multiprocessed loading.
                            # If dataset_num_workers < 1, processing will take place on the main process.
@@ -93,6 +93,8 @@ tracker:
   enable_wandb: False
   tensorboard:                    # active when enable_tensorboard=True
     log_dir: '_results/logs/runs' # For TensorBoard summary
+    exp_name: null                # Only set this if you want to set an experiment set name specifically for TB
+    log_fname: null               # Only set this if you want to set a unique ID specifically for TB
   wandb:                          # active when enable_wandb=True
     project: 'wisp-nerf'
     entity: null                  # i.e. your wandb username here

app/nerf/configs/nerf_triplanar.yaml
@@ -39,7 +39,7 @@ nef:  # Neural field: combines grid, decoders and positional embedders
 
 tracer: # A tracer for intersecting rays with the blas, generating samples along rays for the neural field & integrating
   constructor: 'PackedRFTracer'
-  bg_color: 'white'
+  bg_color: [ 1.0, 1.0, 1.0 ]
   num_steps: 512   # Samples within the triplanar volume (e.g. AABB)
   raymarch_type: 'voxel'
   step_size: 1.0
@@ -48,7 +48,7 @@ dataset:  # Train & validation dataset
   constructor: 'NeRFSyntheticDataset'
   dataset_path: 'data/'
   split: 'train'      # Which subset of the dataset to use for training
-  bg_color: 'white'   # The background color to use for when alpha=0.
+  bg_color: [ 1.0, 1.0, 1.0 ]   # The background color to use for when alpha=0.
   mip: 0              # If provided, will rescale images by 2**mip. Useful when large images are loaded.
   dataset_num_workers: -1  # The number of workers to spawn for multiprocessed loading.
                            # If dataset_num_workers < 1, processing will take place on the main process.
@@ -93,6 +93,8 @@ tracker:
   enable_wandb: False
   tensorboard:                    # active when enable_tensorboard=True
     log_dir: '_results/logs/runs' # For TensorBoard summary
+    exp_name: null                # Only set this if you want to set an experiment set name specifically for TB
+    log_fname: null               # Only set this if you want to set a unique ID specifically for TB
   wandb:                          # active when enable_wandb=True
     project: 'wisp-nerf'
     entity: null                  # i.e. your wandb username here

app/nerf/main_nerf.py
@@ -65,7 +65,7 @@ cfg = parse_config(NeRFAppConfig, yaml_arg='--config')  # Obtain args by priorit
 device = torch.device(cfg.device)
 default_log_setup(cfg.log_level)
 if cfg.interactive:
-    cfg.tracer.bg_color = 'black'
+    cfg.tracer.bg_color = (0.0, 0.0, 0.0)
     cfg.trainer.render_every = -1
     cfg.trainer.save_every = -1
     cfg.trainer.valid_every = -1
@@ -81,7 +81,7 @@ dataset_transform = instantiate(cfg.dataset_transform)  # SampleRays creates bat
 train_dataset = instantiate(cfg.dataset, transform=dataset_transform)  # A Multiview dataset
 validation_dataset = None
 if cfg.trainer.valid_every > -1 or cfg.trainer.mode == 'validate':
-    validation_dataset = train_dataset.create_split(split='val', transform=None)
+    validation_dataset = train_dataset.create_split(split=cfg.trainer.valid_split, transform=None)
 
 if cfg.pretrained and cfg.trainer.model_format == "full":
     pipeline = torch.load(cfg.pretrained)   # Load a full pretrained pipeline: model + weights

app/nglod/configs/nglod_hash.yaml
@@ -91,6 +91,8 @@ tracker:
   enable_wandb: False
   tensorboard:
     log_dir: '_results/logs/runs' # For TensorBoard summary
+    exp_name: null                # Only set this if you want to set an experiment set name specifically for TB
+    log_fname: null               # Only set this if you want to set a unique ID specifically for TB
   wandb:                          # active when enable_wandb=True
     project: 'wisp-nglod'
     entity: null                  # i.e. your wandb username here

commit e4dfb67163718b906e9637bfe7a4c556b592077d

RenderCore fix to needs_refresh (#174)
Fix interactive visualizer refresh issue introduced in #167

Signed-off-by: operel <operel@nvidia.com>

wisp/renderer/core/render_core.py
@@ -398,12 +398,10 @@ class RendererCore:
             return True
 
         # Resolution check: if not full resolution - canvas is dirty
-        #if self._last_state['res_x'] != self.camera.width or self._last_state['res_y'] != self.camera.height:
-        #    return True
+        if self._last_state['res_x'] != self.camera.width or self._last_state['res_y'] != self.camera.height:
+            return True
 
         for att_name, prev_val in self._last_state.items():
-            if att_name in ['res_x', 'res_y']:
-                continue
             if not hasattr(self, att_name):
                 continue
             curr_val = self.__getattribute__(att_name)